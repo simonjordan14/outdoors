62efeff0999284900dd7d9c364cc70cfe4f41272
simonjordan14 <simon_jordan@live.com>
2024-03-12 14:50:52 +0100

    included env file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
